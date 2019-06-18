e22a434138f0770478d8652df0cf0e1e09b50c14
asbird <ptasinskiwojciech@gmail.com>
2019-06-19 00:36:41 +0200

    Added feature scalling function

--- LinearRegr.py
+++ LinearRegr.py
@@ -1,20 +1,43 @@
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import pdb
 
 
-df = pd.read_csv('house_data.csv', sep=',')
-corr = df.corr()
 
-ax = sns.heatmap(corr, cmap='coolwarm_r',center=0)
-plt.show()
 
-params = ['sqft_living', 'grade', 'sqft_above', 'sqft_living15', 'bathrooms']
+# corr = df.corr()
+#
+# ax = sns.heatmap(corr, cmap='coolwarm_r',center=0)
+# plt.show()
+#
 # Iteration through Data frame
 # for index, row in df.iterrows():
 #     print(row['id'])
+def h_x(theta, x):
+    theta = theta.transpose()
+    return np.matmul(theta,x)
 
+def FeatureScalling(X):
+    for index, row in X.iterrows():
+        for feature in list(X):
+            _prev = X.at[index,feature]
+            _mean = X[feature].mean()
+            _max = X[feature].max()
+            _min = X[feature].min()
+            _current = (_prev - _mean) / (_max - _min)
+            X.at[index,feature] = _current
 
-def h_x(params, x):
-    print("xd")
+            # DEBUGGING
+            # print(str(index) +': Feature:' + str(feature) +
+            # ' Prev: '+str(_prev)+' Current: '+str(_current))
+
+df = pd.read_csv('house_data.csv', sep=',')
+features = ['sqft_living', 'grade', 'sqft_above', 'sqft_living15', 'bathrooms']
+
+X = df[features]
+y = df[['price']]
+
+FeatureScalling(X)
